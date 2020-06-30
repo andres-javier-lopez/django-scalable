f60608f436f17e43d36dc4f06b3020a95cd0019c
Andrés Javier López <ajlopez@applaudostudios.com>
2020-06-30 10:13:53 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name='bookstore',
+    version='0.0.0',
+    author='Andres Javier Lopez',
+    author_email='ajavier.lopez@gmail.com',
+    description='A sample bookstore project',
+    packages=setuptools.find_packages(),
+    scripts=['manage.py'],
+)
